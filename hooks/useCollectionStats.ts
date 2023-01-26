0396789977d9c0ee76ca67155ac9b29c6243526a
Ryuzaki01 <wagmiarmy.dev@gmail.com>
2023-01-26 15:34:57 +0700

    + Cleanup

--- hooks/useCollectionStats.ts
+++ hooks/useCollectionStats.ts
@@ -2,12 +2,9 @@ import { paths } from '@reservoir0x/reservoir-sdk'
 import fetcher from 'lib/fetcher'
 import setParams from 'lib/params'
 import { NextRouter } from 'next/router'
-import moment from 'moment';
 import useSWR from 'swr'
-import { ethers } from 'ethers'
 
 const PROXY_API_BASE = process.env.NEXT_PUBLIC_PROXY_API_BASE
-const PROXY_API_NFT_BASE = process.env.NEXT_PUBLIC_NFT_PROXY_API_BASE
 
 export default function useCollectionStats(
   router: NextRouter,
@@ -43,50 +40,17 @@ export default function useCollectionStats(
       })
     }
 
-    return setParams(pathname, { ...query, ...query2 })
+    const href = setParams(pathname, { ...query, ...query2 })
+
+    return href
   }
 
   const href = getUrl()
 
-  const modifiedFetcher = async (href: string) => {
-    const response = await fetcher(href);
-    const { listings } = await fetch(`${PROXY_API_NFT_BASE}/api/v0/nfts/listings/optimism/${collectionId}?order_by=price_asc&limit=1`)
-      .then(res => res.json())
-      .catch(() => {});
-
-
-    // {
-    //   tokenCount: number;
-    //   onSaleCount: number;
-    //   flaggedTokenCount: number;
-    //   sampleImages?: definitions["sampleImages"];
-    //   market?: definitions["Model54"];
-    // }
-
-    if (listings && listings[0]) {
-      response.stats.market.floorAsk = {
-        ...response.stats.market.floorAsk,
-        id: listings[0].id,
-        price: {
-          amount: {
-            raw: listings[0].price,
-            decimal: ethers.BigNumber.from(listings[0].price),
-            native: ethers.BigNumber.from(listings[0].price),
-          },
-          netAmount: listings[0].price,
-          currency: listings[0].payment_token.symbol,
-        },
-        maker: listings[0].seller_address,
-        validFrom: moment(listings[0].listing_timestamp).unix(),
-        validUntil: moment(listings[0].expiration_timestamp).unix()
-      };
-    }
-
-    return response;
-  }
-
-  return useSWR<paths['/stats/v2']['get']['responses']['200']['schema']>(
+  const stats = useSWR<paths['/stats/v2']['get']['responses']['200']['schema']>(
     href,
-    modifiedFetcher
+    fetcher
   )
+
+  return stats
 }
